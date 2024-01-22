6db8cae51c19c48a8df211151992f73233b0b20d
root <root@ip-172-31-47-127.ap-south-1.compute.internal>
2024-01-22 07:15:36 +0000

    Flutterflow CICD completed

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -42,7 +42,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
           title: Text(
-            'Sample Application',
+            'Flutterflow CICD',
             style: FlutterFlowTheme.of(context).headlineMedium.override(
                   fontFamily: 'Outfit',
                   color: Colors.white,
