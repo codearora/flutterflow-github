f5337b8af03f06d709e7897a43ed1756472cd5e6
root <root@ip-172-31-47-127.ap-south-1.compute.internal>
2024-01-23 05:49:25 +0000

    changed title

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -42,7 +42,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
           title: Text(
-            'Flutterflow CICD',
+            'Flutterflow CICD testing',
             style: FlutterFlowTheme.of(context).headlineMedium.override(
                   fontFamily: 'Outfit',
                   color: Colors.white,
